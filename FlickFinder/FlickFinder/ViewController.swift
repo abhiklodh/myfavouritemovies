d015c894f6a9cc0376f5a0ce5e5d5cfe4e7f6c89
Jarrod Parkes <jarrod@udacity.com>
2015-02-04 14:06:51 -0800

    changes for step-2.9

--- FlickFinder/FlickFinder/ViewController.swift
+++ FlickFinder/FlickFinder/ViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 let BASE_URL = "https://api.flickr.com/services/rest/"
 let METHOD_NAME = "flickr.photos.search"
-let API_KEY = "ce190e05b11ca689a9c1fac8c9de619d"
+let API_KEY = "ENTER_YOUR_API_KEY_HERE"
 let EXTRAS = "url_m"
 let SAFE_SEARCH = "1"
 let DATA_FORMAT = "json"
 let NO_JSON_CALLBACK = "1"
 let BOUNDING_BOX_HALF_WIDTH = 1.0
 let BOUNDING_BOX_HALF_HEIGHT = 1.0
+let LAT_MIN = -90.0
+let LAT_MAX = 90.0
+let LON_MIN = -180.0
+let LON_MAX = 180.0
+
+extension String {
+    func toDouble() -> Double? {
+        return NSNumberFormatter().numberFromString(self)?.doubleValue
+    }
+}
 
 class ViewController: UIViewController {
 
@@ -30,29 +40,55 @@ class ViewController: UIViewController {
     var tapRecognizer: UITapGestureRecognizer? = nil
     
     @IBAction func searchPhotosByPhraseButtonTouchUp(sender: AnyObject) {
-        let methodArguments = [
-            "method": METHOD_NAME,
-            "api_key": API_KEY,
-            "text": self.phraseTextField.text,
-            "safe_search": SAFE_SEARCH,
-            "extras": EXTRAS,
-            "format": DATA_FORMAT,
-            "nojsoncallback": NO_JSON_CALLBACK
-        ]
-        getImageFromFlickrBySearch(methodArguments)
+        if !self.phraseTextField.text.isEmpty {
+            self.photoTitleLabel.text = "Searching..."
+            let methodArguments = [
+                "method": METHOD_NAME,
+                "api_key": API_KEY,
+                "text": self.phraseTextField.text,
+                "safe_search": SAFE_SEARCH,
+                "extras": EXTRAS,
+                "format": DATA_FORMAT,
+                "nojsoncallback": NO_JSON_CALLBACK
+            ]
+            getImageFromFlickrBySearch(methodArguments)
+        } else {
+            self.photoTitleLabel.text = "Phrase Empty."
+        }
     }
     
     @IBAction func searchPhotosByLatLonButtonTouchUp(sender: AnyObject) {
-        let methodArguments = [
-            "method": METHOD_NAME,
-            "api_key": API_KEY,
-            "bbox": createBoundingBoxString(),
-            "safe_search": SAFE_SEARCH,
-            "extras": EXTRAS,
-            "format": DATA_FORMAT,
-            "nojsoncallback": NO_JSON_CALLBACK
-        ]
-        getImageFromFlickrBySearch(methodArguments)
+        if !self.latitudeTextField.text.isEmpty && !self.longitudeTextField.text.isEmpty {
+            if validLatitude() && validLongitude() {
+                self.photoTitleLabel.text = "Searching..."
+                let methodArguments = [
+                    "method": METHOD_NAME,
+                    "api_key": API_KEY,
+                    "bbox": createBoundingBoxString(),
+                    "safe_search": SAFE_SEARCH,
+                    "extras": EXTRAS,
+                    "format": DATA_FORMAT,
+                    "nojsoncallback": NO_JSON_CALLBACK
+                ]
+                getImageFromFlickrBySearch(methodArguments)
+            } else {
+                if !validLatitude() && !validLongitude() {
+                    self.photoTitleLabel.text = "Lat/Lon Invalid.\nLat should be [-90, 90].\nLon should be [-180, 180]."
+                } else if !validLatitude() {
+                    self.photoTitleLabel.text = "Lat Invalid.\nLat should be [-90, 90]."
+                } else {
+                    self.photoTitleLabel.text = "Lon Invalid.\nLon should be [-180, 180]."
+                }
+            }
+        } else {
+            if self.latitudeTextField.text.isEmpty && self.longitudeTextField.text.isEmpty {
+                self.photoTitleLabel.text = "Lat/Lon Empty."
+            } else if self.latitudeTextField.text.isEmpty {
+                self.photoTitleLabel.text = "Lat Empty."
+            } else {
+                self.photoTitleLabel.text = "Lon Empty."
+            }
+        }
     }
     
     override func viewDidLoad() {
@@ -136,6 +172,30 @@ class ViewController: UIViewController {
         return "\(longitude - BOUNDING_BOX_HALF_WIDTH),\(latitude - BOUNDING_BOX_HALF_HEIGHT),\(longitude + BOUNDING_BOX_HALF_WIDTH),\(latitude + BOUNDING_BOX_HALF_HEIGHT)"
     }
     
+    /* Check to make sure the latitude falls within [-90, 90] */
+    func validLatitude() -> Bool {
+        if let latitude : Double? = self.latitudeTextField.text.toDouble() {
+            if latitude < LAT_MIN || latitude > LAT_MAX {
+                return false
+            }
+        } else {
+            return false
+        }
+        return true
+    }
+    
+    /* Check to make sure the longitude falls within [-180, 180] */
+    func validLongitude() -> Bool {
+        if let longitude : Double? = self.longitudeTextField.text.toDouble() {
+            if longitude < LON_MIN || longitude > LON_MAX {
+                return false
+            }
+        } else {
+            return false
+        }
+        return true
+    }
+    
     func getLatLonString() -> String {
         let latitude = (self.latitudeTextField.text as NSString).doubleValue
         let longitude = (self.longitudeTextField.text as NSString).doubleValue
@@ -161,7 +221,6 @@ class ViewController: UIViewController {
                 
                 if let photosDictionary = parsedResult.valueForKey("photos") as? [String:AnyObject] {
                     
-                    /* 1 - Get random page, then make new request */
                     if let totalPages = photosDictionary["pages"] as? Int {
                         
                         /* Flickr API - will only return up the 4000 images (100 per page * 40 page max) */
@@ -172,6 +231,8 @@ class ViewController: UIViewController {
                     } else {
                         println("Cant find key 'pages' in \(photosDictionary)")
                     }
+                } else {
+                    println("Cant find key 'photos' in \(parsedResult)")
                 }
             }
         }
@@ -181,7 +242,7 @@ class ViewController: UIViewController {
 
     func getImageFromFlickrBySearchWithPage(methodArguments: [String : AnyObject], pageNumber: Int) {
         
-        /* 2 - Add the page to the method's arguments */
+        /* Add the page to the method's arguments */
         var withPageDictionary = methodArguments
         withPageDictionary["page"] = pageNumber
         
@@ -190,7 +251,6 @@ class ViewController: UIViewController {
         let url = NSURL(string: urlString)!
         let request = NSURLRequest(URL: url)
         
-        /* 3 - Same request as before */
         let task = session.dataTaskWithRequest(request) {data, response, downloadError in
             if let error = downloadError? {
                 println("Could not complete the request \(error)")
