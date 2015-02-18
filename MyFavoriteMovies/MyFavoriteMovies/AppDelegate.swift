1b0d4bbf732618c70591e52a45b4e535acd2e716
Jarrod Parkes <jarrod@udacity.com>
2015-02-18 09:11:12 -0800

    adding init files for favorite movies

---
+++ MyFavoriteMovies/MyFavoriteMovies/AppDelegate.swift
@@ -0,0 +1,65 @@
+//
+//  AppDelegate.swift
+//  MyFavoriteMovies
+//
+//  Created by Jarrod Parkes on 1/23/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    /* Constants for TheMovieDB */
+    let apiKey = "3c534dc71a2f9d28e9594e5773abcc93"
+    let baseURLString = "http://api.themoviedb.org/3/"
+    let baseURLSecureString = "https://api.themoviedb.org/3/"
+    
+    /* Need these for login */
+    var requestToken: String? = nil
+    var sessionID: String? = nil
+    var userID: Int? = nil
+
+    /* Configuration for TheMovieDB, we'll take care of this for you =)... */
+    var config = Config()
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        /* If necessary, update the configuration */
+        config.updateIfDaysSinceUpdateExceeds(7)
+        
+        return true
+    }
+}
+
+// MARK: - Helper
+
+extension AppDelegate {
+    
+    /* Helper function: Given a dictionary of parameters, convert to a string for a url */
+    func escapedParameters(parameters: [String : AnyObject]) -> String {
+        
+        var urlVars = [String]()
+        
+        for (key, value) in parameters {
+            
+            /* Make sure that it is a string value */
+            let stringValue = "\(value)"
+            
+            /* Escape it */
+            let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+            
+            /* FIX: Replace spaces with '+' */
+            let replaceSpaceValue = stringValue.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.LiteralSearch, range: nil)
+            
+            /* Append it */
+            urlVars += [key + "=" + "\(replaceSpaceValue)"]
+        }
+        
+        return (!urlVars.isEmpty ? "?" : "") + join("&", urlVars)
+    }
+}
+
