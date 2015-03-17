f13ddd12f1095b13bab1c6c0080a1ccd36a971dc
Jarrod Parkes <jarrod@udacity.com>
2015-03-16 17:39:17 -0700

    renaming auth view

--- TheMovieManager/TheMovieManager/TMDBConvenience.swift
+++ TheMovieManager/TheMovieManager/TMDBConvenience.swift
@@ -89,12 +89,12 @@ extension TMDBClient {
         }
     }
     
-    /* This function opens a WebAuthViewController to handle Step 2a of the auth flow */
+    /* This function opens a TMDBAuthViewController to handle Step 2a of the auth flow */
     func loginWithToken(requestToken: String?, hostViewController: UIViewController, completionHandler: (success: Bool, errorString: String?) -> Void) {
 
         let authorizationURL = NSURL(string: "\(TMDBClient.Constants.AuthorizationURL)\(requestToken!)")
         let request = NSURLRequest(URL: authorizationURL!)
-        let webAuthViewController = hostViewController.storyboard!.instantiateViewControllerWithIdentifier("WebAuthViewController") as WebAuthViewController
+        let webAuthViewController = hostViewController.storyboard!.instantiateViewControllerWithIdentifier("TMDBAuthViewController") as TMDBAuthViewController
         webAuthViewController.urlRequest = request
         webAuthViewController.requestToken = requestToken
         webAuthViewController.completionHandler = completionHandler
